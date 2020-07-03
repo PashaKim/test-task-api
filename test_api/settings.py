afde1cf0b5d509daa8416468fb7367da7505ae30
pavlo <pavlo.chubarov.ua@gmail.com>
2020-07-03 12:29:14 +0300

    Add db dump

--- test_api/settings.py
+++ test_api/settings.py
@@ -136,10 +136,6 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'content.api.authentication.ExpiringTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication'
+        'rest_framework.authentication.TokenAuthentication', 
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
 }
